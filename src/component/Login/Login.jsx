dea3607c09773e86535e6afaf69279fb44a4296d
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2023-10-01 20:46:10 +0600

    UI and logic for Log in page is done

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,7 +1,87 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { Link } from "react-router-dom";
+import auth from "../firebase/firebase.init";
+import { useState } from "react";
+
 const Login = () => {
+  const [registerError, setRegisterError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        setSuccess("User logged in successfully");
+      })
+      .catch((error) => {
+        setRegisterError(error.message);
+      });
+  };
   return (
-    <div className="text-center py-8">
-      <h2 className="text-3xl">Log in</h2>,
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <form onSubmit={handleLogin}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              {registerError && (
+                <p className="text-red-800 mb-4 font-bold">{registerError}</p>
+              )}
+              {success && (
+                <p className="text-green-800 mb-4 font-bold">{success}</p>
+              )}
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+            <p>
+              Do not have any account? Please{" "}
+              <Link className="text-blue-600" to="/register">
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
